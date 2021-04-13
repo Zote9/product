c051ce661e230a9a43aa46c771fb38911ff90336
郁翔 <82081473+Zote9@users.noreply.github.com>
2021-04-13 16:03:51 +0800

    add for loop products

--- products.py
+++ products.py
@@ -14,6 +14,9 @@ while True:
 
 	products.append([goods, price])
 
-print(products)
-print(products[0])
-print(products[0][0])
+# print(products)
+# print(products[0])
+# print(products[0][0])
+for product in products:
+	print('%s 的價格為 %s 元' % (product[0], product[1]))
+
