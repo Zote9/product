699405b22e5da3eb6c29d4f2a160cf1e6fd51c5b
郁翔 <82081473+Zote9@users.noreply.github.com>
2021-04-13 19:37:02 +0800

    add products

--- products.py
+++ products.py
@@ -20,3 +20,7 @@ while True:
 for product in products:
 	print('%s 的價格為 %s 元' % (product[0], product[1]))
 
+with open('products.csv', 'w', encoding = 'utf-8') as f:
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
